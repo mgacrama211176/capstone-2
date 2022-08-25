45c613647634b85f70155e25afbc63c623eb6c33
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-08-26 01:31:48 +0800

    Push

--- frontend/src/media.jsx
+++ frontend/src/media.jsx
@@ -11,13 +11,13 @@ const size = {
 };
 
 export const device = {
-  mobileS: `only screen and (min-width: ${size.mobileS}) { ... }`,
-  mobileM: `only screen and (min-width: ${size.mobileM}){ ... }`,
-  mobileL: `only screen and (max-width: ${size.mobileL}){ ... }`,
-  tablet: `only screen and (max-width: ${size.tablet}){ ... }`,
-  laptop: `only screen and (max-width: ${size.laptop}){ ... }`,
-  laptopL: `only screen and (max-width: ${size.laptopL}){ ... }`,
-  desktopL: `only screen and (max-width: ${size.desktopL}){ ... }`,
+  mobileS: `only screen and (min-width: ${size.mobileS}) `,
+  mobileM: `only screen and (min-width: ${size.mobileM})`,
+  mobileL: `only screen and (max-width: ${size.mobileL})`,
+  tablet: `only screen and (max-width: ${size.tablet})`,
+  laptop: `only screen and (max-width: ${size.laptop})`,
+  laptopL: `only screen and (max-width: ${size.laptopL})`,
+  desktopL: `only screen and (max-width: ${size.desktopL})`,
 };
 
 const media = () => {
