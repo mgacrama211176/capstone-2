c485179d255406ffbfba4617c02411a07ee3f3da
Marlon Gacrama Jr <mgacrama_ccs@uspf.edu.ph>
2022-09-07 05:54:38 +0800

    update

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import tile from '../assets/home_post_2.gif';
-import { device } from '../media';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import tile from "../assets/home_post_2.gif";
+import { device } from "../media";
 
 //libraries
-import { format } from 'timeago.js';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { format } from "timeago.js";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Container = styled.div`
-  max-width: ${(props) => props.type !== 'sm' && '360px'};
-  margin-bottom: ${(props) => (props.type === 'sm' ? '10px' : '45px')};
-  display: ${(props) => (props.type === 'sm' ? 'flex' : '')};
+  max-width: ${(props) => props.type !== "sm" && "360px"};
+  margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45px")};
+  display: ${(props) => (props.type === "sm" ? "flex" : "")};
   cursor: pointer;
   flex-wrap: wrap;
 
   /* Mobile S [fixed]*/
   @media ${device.mobileS} {
     gap: 5px;
-    margin: ${(props) => (props.type === 'sm' ? '15px 0px' : '0px')};
-    max-width: ${(props) => (props.type === 'sm' ? '340px' : '300px')};
-    gap: ${(props) => (props.type === 'sm' ? '15px' : '0px')};
+    margin: ${(props) => (props.type === "sm" ? "15px 0px" : "0px")};
+    max-width: ${(props) => (props.type === "sm" ? "340px" : "300px")};
+    gap: ${(props) => (props.type === "sm" ? "15px" : "0px")};
   }
 
   /* Mobile M */
   @media ${device.mobileM} {
     gap: 5px;
-    margin: ${(props) => (props.type === 'sm' ? '15px 0px' : '0px')};
-    max-width: ${(props) => (props.type === 'sm' ? '340px' : '300px')};
-    gap: ${(props) => (props.type === 'sm' ? '15px' : '0px')};
+    margin: ${(props) => (props.type === "sm" ? "15px 0px" : "0px")};
+    max-width: ${(props) => (props.type === "sm" ? "340px" : "300px")};
+    gap: ${(props) => (props.type === "sm" ? "15px" : "0px")};
   }
 
   /* Mobile L */
   @media ${device.mobileL} {
     gap: 5px;
-    margin: ${(props) => (props.type === 'sm' ? '15px 0px' : '0px')};
-    max-width: ${(props) => (props.type === 'sm' ? '420px' : '300px')};
-    gap: ${(props) => (props.type === 'sm' ? '15px' : '0px')};
+    margin: ${(props) => (props.type === "sm" ? "15px 0px" : "0px")};
+    max-width: ${(props) => (props.type === "sm" ? "420px" : "300px")};
+    gap: ${(props) => (props.type === "sm" ? "15px" : "0px")};
   }
 
   /* Tablet */
@@ -53,15 +53,15 @@ const Container = styled.div`
 `;
 
 const Image = styled.img`
-  width: ${(props) => (props.type === 'sm' ? '15em' : '18em')};
-  height: ${(props) => (props.type === 'sm' ? '120px' : '202px')};
+  width: ${(props) => (props.type === "sm" ? "15em" : "18em")};
+  height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
   background-color: #999;
   flex: 1;
 `;
 
 const Details = styled.div`
   display: flex;
-  margin-top: ${(props) => (props.type === 'sm' ? '16px' : '0px')};
+  margin-top: ${(props) => (props.type === "sm" ? "16px" : "0px")};
   gap: 12px;
   font-family: Inter;
   flex: 1;
@@ -70,24 +70,24 @@ const Details = styled.div`
   /* Mobile S [fixed]*/
   @media ${device.mobileS} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
 
   /* Mobile M */
   @media ${device.mobileM} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
 
   /* Mobile L */
   @media ${device.mobileL} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
   /* Desktop */
   @media ${device.desktop} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
 `;
 
@@ -96,23 +96,23 @@ const ChannelImage = styled.img`
   height: 36px;
   border-radius: 50%;
   background-color: #999;
-  display: ${(props) => props.type === 'sm' && 'none'};
+  display: ${(props) => props.type === "sm" && "none"};
 `;
 
 const Texts = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  min-width: ${(props) => (props.type === 'sm' ? '0px' : '300px')};
+  min-width: ${(props) => (props.type === "sm" ? "0px" : "300px")};
 
   /* Mobile S [fixed]*/
   @media ${device.mobileS} {
-    max-width: ${(props) => (props.type === 'sm' ? '200px' : '0px')};
+    max-width: ${(props) => (props.type === "sm" ? "200px" : "0px")};
   }
 `;
 
 const Title = styled.h1`
-  font-size: ${(props) => props.type !== 'sm' && '12px'};
+  font-size: ${(props) => props.type !== "sm" && "12px"};
   font-weight: 500;
   color: ${({ theme }) => theme.titleColor};
   display: flex;
@@ -138,22 +138,22 @@ const Card = ({ type, video }) => {
       const channel = await axios.get(
         `http://localhost:4000/api/users/find/${video.userId}`
       );
-      console.log(video.userId);
-      // console.log(channel);
-      // setChannel(channel.data.username);
+      // console.log(video.userId);
+      console.log(channel.data.image);
+      setChannel(channel.data);
     };
     fetchingChannel();
   }, [video.userId]);
 
   return (
-    <Link to="/video/test" style={{ textDecoration: 'none' }}>
+    <Link to="/video/test" style={{ textDecoration: "none" }}>
       <Container type={type}>
         <Image type={type} src={video.imgUrl} />
         <Details type={type}>
           {/* <ChannelImage type={type} src={channel.image} /> */}
           <Texts type={type}>
             <Title>{video.title}</Title>
-            {/* <AnimatorName>{channel.data.username}</AnimatorName> */}
+            <AnimatorName>{channel.username}</AnimatorName>
             <Info>
               {video.views} views • {format(video.createdAt)}
             </Info>
