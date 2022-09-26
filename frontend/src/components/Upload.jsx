e45e5d84655ced7ad6f5704d3eaf4f3b52dac2ce
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-09-26 08:49:09 +0800

    upload update

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,6 +1,14 @@
 import { height } from '@mui/system';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+
+//firebase/store
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
 
 const Container = styled.div`
   width: 100%;
@@ -92,6 +100,65 @@ const Label = styled.label``;
 const Upload = ({ setOpenModal }) => {
   const [thumbnail, setThumbnail] = useState(undefined);
   const [video, setVideo] = useState(undefined);
+  const [thumbnailPercentage, setThumbnailPercentage] = useState(0);
+  const [videoPercentage, setVideoPercentage] = useState(0);
+
+  const [uploadInformation, setUploadInformation] = useState({
+    title: '',
+    desc: '',
+    tags: 'Traditional Animation',
+  });
+
+  const onChangeHandleInformation = (e) => {
+    const newInfo = { ...uploadInformation };
+    newInfo[e.target.id] = e.target.value;
+    console.log(newInfo);
+    setUploadInformation(newInfo);
+  };
+
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage();
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === 'imgUrl'
+          ? setThumbnailPercentage(progress)
+          : setVideoPercentage(progress);
+        switch (snapshot.state) {
+          case 'paused':
+            console.log(`Upload is paused`);
+            break;
+          case 'running':
+            console.log(`Upload is running`);
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {},
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setUploadInformation((prev) => {
+            return { ...prev, [urlType]: downloadURL };
+          });
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    uploadFile(video);
+  }, [video]);
+
+  useEffect(() => {
+    uploadFile(thumbnail);
+  }, [thumbnail]);
 
   return (
     <Container>
@@ -99,10 +166,29 @@ const Upload = ({ setOpenModal }) => {
         <Close onClick={() => setOpenModal(false)}>X</Close>
 
         <Title>Upload video</Title>
-        <Input type="file" accept="video/*" onChange={(e) => SetVideo()} />
-        <Input type="text" placeholder="Title" id="title" />
-        <TextArea placeholder="Description" rows={8}></TextArea>
-        <Select name="tags" id="tags">
+        <Input
+          type="file"
+          accept="video/*"
+          onChange={(e) => SetVideo(e.target.files[0])}
+        />
+        <Input
+          type="text"
+          placeholder="Title"
+          id="title"
+          onChange={(e) => onChangeHandleInformation(e)}
+        />
+        <TextArea
+          placeholder="Description"
+          rows={8}
+          id="desc"
+          onChange={(e) => onChangeHandleInformation(e)}
+        ></TextArea>
+
+        <Select
+          name="tags"
+          id="tags"
+          onChange={(e) => onChangeHandleInformation(e)}
+        >
           <Option value="Traditional Animation">Traditional Animation</Option>
           <Option value="2D animation">2D animation</Option>
           <Option value="3D animation">3D animation</Option>
@@ -114,7 +200,7 @@ const Upload = ({ setOpenModal }) => {
           type="file"
           accept="image/*"
           placeholder="Thumbnail"
-          onChange={(e) => setThumbnail()}
+          onChange={(e) => setThumbnail(e.target.files[0])}
         />
         <Button>Upload</Button>
       </Wrapper>
