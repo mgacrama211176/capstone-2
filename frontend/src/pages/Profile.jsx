f4481b7d837a23f803f084a136756b597dfa3479
Steve Bryan Suico <ssuico78@gmail.com>
2022-10-13 16:25:04 +0800

    profile30%

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -22,121 +22,85 @@ import Tab from "@mui/material/Tab";
 
 import axios from "axios";
 
-const ProfileWrapper = styled.div`
-  background-color: white;
-  border-radius: 6px;
-  width: 20%;
-  height: 50%;
-  display: inline-block;
-  border: 1px solid black;
-  margin: 5px;
-  padding: 0px;
+const MainWrapper = styled.div`
+  /* background-color */
+  background:linear-gradient(90deg, rgba(18, 42, 177, 1) 0%, rgba(94, 111, 207, 1) 50%, rgba(112, 44, 147, 1) 100%);
   display: flex;
+  border-radius: 25px;
+  color: white;
+  font-family: Roboto, Arial, sans-serif;
+  margin: 100%;
+  padding 0;
+  margin-top: 10px;
+  margin-left: 80px;
+  margin-right: 80px;
+  align-items: center;
+  justify-content: center;
+
+  
+  
+`;
+
+const ProfileWrapper = styled.div`
+background:linear-gradient(90deg, rgba(188, 0, 255, 1) 0%, rgba(36, 119, 187, 1) 50%, rgba(14, 9, 197, 1) 100%);
+  display: flex;
+  border-radius: 25px;
+  color: white;
+  font-family: Roboto, Arial, sans-serif;
+  margin: 100%;
+  padding 0;
+  margin-top: 10px;
+  margin-left: 80px;
+  margin-right: 80px;
+  align-items: center;
+ `;
+
+const Videowrapper = styled.div`
+  background: linear-gradient(
+    90deg,
+    rgba(188, 0, 255, 1) 0%,
+    rgba(36, 119, 187, 1) 50%,
+    rgba(14, 9, 197, 1) 100%
+  );
+
+  display: flex;
+  border-radius: 25px;
+  color: white;
+  font-family: Roboto, Arial, sans-serif;
+  margin: 100%;
+  padding 0;
+  margin-top: 10px;
+  margin-left: 80px;
+  margin-right: 80px;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ImgCon = styled.figure`
-  width: 50%;
-  height: 50%;
-  display: flex;
-  margin-left: 20%;
-  align-items: center;
-  object-fit: cover;
-  clip-path: circle(45% at 50% 50%);
+  margin-top: 0.5%;
 
-  /* <clipPath id="svgPath" clipPathUnits="objectBoundingBox">
-            <circle r="0.05" cy="0.0625" cx="0.1625" />
-            <circle r="0.09322" cy="0.29375" cx="0.2424" /> 
-            <!-- rest of path here-->
-        </clipPath> */
+  object-fit: cover;
+  width: 90px;
+  -webkit-clip-path: circle(46% at 50% 50%);
+  clip-path: circle(46% at 50% 50%);
 `;
 
 const Pimg = styled.img`
-  width: 100%;
-  height: 100%;
   object-fit: cover;
-
-  &:focus img {
-    -webkit-transition: all 1s ease;
-    -moz-transition: all 1s ease;
-    -o-transition: all 1s ease;
-    -ms-transition: all 1s ease;
-    transition: all 1s ease;
-  }
-  &:focus img:hover {
-    border: 70px solid #000;
-    border-radius: 50%;
-    -webkit-transition: all 1s ease;
-    -moz-transition: all 1s ease;
-    -o-transition: all 1s ease;
-    -ms-transition: all 1s ease;
-    transition: all 1s ease;
-  }
 `;
 
 const TypoName = styled.p`
+  padding: 2rem;
   text-decoration: uppercase;
-  font-size: 17px;
-  font-weight: bold;
-  font-family: Roboto, Arial, sans-serif;
-  margin-top: 60%;
+  font-size: 2rem;
   align-content: center;
-  margin-left: -43%;
 `;
 
-const TypoTitle = styled.p`
-  font-size: 13px;
-  color: #777;
-  font-family: Roboto, Arial, sans-serif;
-  margin-top: 70%;
-  align-content: center;
-  margin-left: -43%;
+const TypoDetails = styled.p`
+  padding: 2rem;
 `;
-
-const MainWrapper = styled.div`
-  margin-left: 100px;
-`;
-
-const CVbt = styled.button`
-  appearance: button;
-  background-color: #132550;
-  background-image: none;
-  border: 1px solid #000;
-  border-radius: 4px;
-  box-shadow: #000 4px 4px 0 0, #000 4px 4px 0 1px;
-  box-sizing: border-box;
-  color: #ffff;
-  cursor: pointer;
-  display: inline-block;
-  font-family: Roboto, Arial, sans-serif;
-  font-size: 16px;
-  font-weight: bolder;
-  line-height: 20px;
-  margin: 0 5px 10px 0;
-  overflow: visible;
-  padding: 12px 40px;
-  text-align: center;
-  text-transform: none;
-  touch-action: manipulation;
-  user-select: none;
-  -webkit-user-select: none;
-  vertical-align: middle;
-  white-space: nowrap;
-  margin-left: 40px;
-  margin-bottom: 8px;
-  &:focus {
-    text-decoration: none;
-  }
-  &:hover {
-    text-decoration: none;
-  }
-  &:active {
-    box-shadow: rgba(0, 0, 0, 0.125) 0 3px 5px inset;
-    outline: 0;
-  }
-  &:not([disabled]):active {
-    box-shadow: #fff 2px 2px 0 0, #000 2px 2px 0 1px;
-    transform: translate(2px, 2px);
-  }
+const Typoemail = styled.p`
+  padding: 2rem;
 `;
 
 const Profile = ({ nav }) => {
@@ -162,16 +126,20 @@ const Profile = ({ nav }) => {
         <ImgCon>
           <Pimg src={retrivedUser.image}></Pimg>
         </ImgCon>
+        <Typoemail>{retrivedUser.email}</Typoemail>
         <TypoName>
           {retrivedUser.fullName !== undefined
             ? retrivedUser.fullName
             : retrivedUser.username}
         </TypoName>
-        <TypoTitle>{retrivedUser.email}</TypoTitle>
-        {/* User Category: {retrivedUser.userCategory}
-        Address: {retrivedUser.address}
-        Subscribers: {retrivedUser.subscribers}
-        Subscribed Users: {retrivedUser?.subscribedUsers?.length} */}
+
+        <TypoDetails>User Category {retrivedUser.userCategory}</TypoDetails>
+        <TypoDetails>
+          <br />
+          {/* Address: {retrivedUser.address} */}
+          {retrivedUser.subscribers} Subscribers <br />
+          {retrivedUser?.subscribedUsers?.length} Subscribed Users
+        </TypoDetails>
       </ProfileWrapper>
     </MainWrapper>
   );
