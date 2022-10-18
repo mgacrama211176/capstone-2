8f09a91461e9cca718cd239ccfaaf5221052b9ad
Marlon Gacrama Jr <mgacrama_ccs@uspf.edu.ph>
2022-10-18 23:58:33 +0800

    yawa mani

--- frontend/src/pages/UpdateProfile.jsx
+++ frontend/src/pages/UpdateProfile.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import background from "../assets/Overcome-bro.png";
 const ariaLabel = { "aria-label": "description" };
 import { useSelector } from "react-redux";
-
 import axios from "axios";
+
+import BGimage from "../assets/neon.jpg";
 
 //MUI
 import TextField from "@mui/material/TextField";
 import Input from "@mui/material/Input";
 import InputAdornment from "@mui/material/InputAdornment";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import BrushIcon from "@mui/icons-material/Brush";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 
 const Container = styled.div`
   margin: 0px 100px;
@@ -18,10 +25,15 @@ const Container = styled.div`
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  background-color: white;
+  background-image: url("${BGimage}");
   border: 1px solid black;
   border-radius: 15px;
   padding: 30px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  max-width: 100%;
+  position: relative;
 `;
 
 const H1 = styled.h1``;
@@ -33,8 +45,9 @@ const AccountSet = styled.div`
 `;
 
 const CardContainer = styled.div`
+  position: relative;
   border: 1px solid black;
-  border-radius: 15px;
+  border-radius: 50%;
   width: 25em;
   height: 10%;
   box-shadow: 5px 5px ${({ theme }) => theme.hover};
@@ -60,13 +73,23 @@ const UserInfo = styled.div`
   gap: 10px;
 `;
 
-const UpdateContainer = styled.div``;
+const UpdateContainer = styled.div`
+  display: flex;
+  flex-flow: wrap column;
+  gap: 30px;
+  justify-content: center;
+  align-items: center;
+  background-color: #f3f4f593;
+`;
 
 const InputContainers = styled.div`
-  margin-left: 50px;
+  /* margin-left: 30px;
+  margin-right: 30px; */
+  padding: 0.5em 3em;
   display: flex;
   flex-direction: column;
   gap: 20px;
+  width: 30em;
 `;
 
 const FileUploadContainers = styled.div``;
@@ -79,12 +102,35 @@ const SubmitContainer = styled.div`
 
 const Submit = styled.button``;
 
+const UpdateImageContainer = styled.div`
+  position: absolute;
+  display: block;
+  right: 20px;
+  color: white;
+  border-radius: 50%;
+  border: 1px solid white;
+  padding: 5px;
+  top: 10px;
+  cursor: pointer;
+
+  &:hover {
+    color: #1976d2;
+    border: 1px solid #1976d2;
+  }
+`;
+
+const Select = styled.select``;
+
+const Options = styled.option``;
+
 const UpdateProfile = () => {
   const { currentUser } = useSelector((state) => state.username);
+  const profileRef = useRef(null);
+  const backgroudRef = useRef(null);
+  const cvRef = useRef(null);
 
   const [newData, setNewData] = useState({
     username: "",
-    Email: "",
     userCategory: "",
     fullName: "",
     address: "",
@@ -101,11 +147,10 @@ const UpdateProfile = () => {
   const onClickUpdateSubmit = async (e) => {
     e.preventDefault();
     try {
-      const update = await axios.post(
-        `http://localhost:4000/api/users/${currentUser._id}`,
+      const update = await axios.put(
+        `http://localhost:4000/api/users/${currentUser?._id}`,
         {
           username: newData.username,
-          Email: newData.Email,
           userCategory: newData.userCategory,
           fullName: newData.fullName,
           address: newData.address,
@@ -119,47 +164,84 @@ const UpdateProfile = () => {
     }
   };
 
+  console.log(newData);
+
   return (
     <Container>
       <AccountSet>Account Update</AccountSet>
       <Wrapper>
         <CardContainer>
+          <UpdateImageContainer>
+            <BrushIcon />
+          </UpdateImageContainer>
           <ImageContainer>
-            <CardImage src={currentUser.image} />
+            <CardImage src={currentUser?.image} />
             <UserInfo>
-              <p>{currentUser.username}</p>
-              <p>{currentUser.userCategory}</p>
+              <p>{currentUser?.username}</p>
+              <p>{currentUser?.userCategory}</p>
             </UserInfo>
           </ImageContainer>
         </CardContainer>
+
         <UpdateContainer>
           <InputContainers>
+            {/* DO NOT TOUCH THIS IS FOR EMAIL */}
             <TextField
               disabled
               id="Email"
-              label={currentUser.email}
+              label={currentUser?.email}
               variant="outlined"
               placeholder="Email"
             />
             <TextField
+              sx={{ color: "white", fontWeight: "light" }}
               id="username"
-              label={currentUser.username}
+              label={currentUser?.username}
               variant="outlined"
               placeholder="Channel Name"
               onChange={(e) => onChangeHandle(e)}
             />
 
-            <TextField
+            {/* <TextField
               id="userCategory"
               label={currentUser.userCategory}
               variant="outlined"
               placeholder="Category"
               onChange={(e) => onChangeHandle(e)}
-            />
+            /> */}
+
+            {/* <Select
+              id="userCategory"
+              onChange={(e) => onChangeHandle(e)}
+              value={newData.userCategory}
+            >
+              <MenuItem value="Animator">Animator</MenuItem>
+              <MenuItem value="Employer">Employer</MenuItem>
+            </Select> */}
+
+            {/* <Select
+              labelId="demo-select-small"
+              id="userCategory"
+              label="userCategory"
+              onChange={(e) => onChangeHandle(e)}
+            >
+              <MenuItem value="Animator">Animator</MenuItem>
+              <MenuItem value="Employer">Employer</MenuItem>
+            </Select> */}
+
+            <Select
+              name="userCategory"
+              id="userCategory"
+              onChange={(e) => onChangeHandle(e)}
+            >
+              <Options value="Animation">Animation</Options>
+              <Options value="Employer">Employer</Options>
+            </Select>
+
             {currentUser.fullName ? (
               <TextField
                 id="fullName"
-                label={currentUser.fullName}
+                label={currentUser?.fullName}
                 variant="outlined"
                 placeholder="Full Name"
                 onChange={(e) => onChangeHandle(e)}
@@ -201,14 +283,14 @@ const UpdateProfile = () => {
             />
             <TextField
               id="about"
-              label={`About the ${currentUser.userCategory}`}
+              label={`About the ${currentUser?.userCategory}`}
               variant="outlined"
               multiline
               maxRows={50}
               onChange={(e) => onChangeHandle(e)}
             />
 
-            <FileUploadContainers>
+            {/* <FileUploadContainers>
               <TextField
                 id="ProfileImage"
                 label="Profile Image"
@@ -220,10 +302,28 @@ const UpdateProfile = () => {
                 variant="outlined"
               />
               <TextField id="uploadCV" label="uploadCV" variant="outlined" />
-            </FileUploadContainers>
+            </FileUploadContainers> */}
           </InputContainers>
+
+          <input type="file" />
+
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            {/* <Button>Upload Profile</Button>
+            <Button>Upload Background</Button> */}
+            <Button>Upload CV</Button>
+          </ButtonGroup>
+
           <SubmitContainer>
-            <Submit onClick={onClickUpdateSubmit}>Update</Submit>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={onClickUpdateSubmit}
+            >
+              Submit
+            </Button>
           </SubmitContainer>
         </UpdateContainer>
       </Wrapper>
