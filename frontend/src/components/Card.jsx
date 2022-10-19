69685037c625d89dd24339bf221a9d48887dd83e
Marlon Gacrama Jr <mgacrama_ccs@uspf.edu.ph>
2022-10-19 21:40:25 +0800

    Fixed Cards

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import styled from 'styled-components';
-import tile from '../assets/home_post_2.gif';
-import { device } from '../media';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import tile from "../assets/home_post_2.gif";
+import { device } from "../media";
 
 //libraries
-import { format } from 'timeago.js';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { format } from "timeago.js";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 //Framer Motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Container = styled.div`
-  max-width: ${(props) => props.type !== 'sm' && '360px'};
-  margin-bottom: ${(props) => (props.type === 'sm' ? '10px' : '45px')};
-  display: ${(props) => (props.type === 'sm' ? 'flex' : '')};
+  max-width: ${(props) => props.type !== "sm" && "300px"};
+  margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45px")};
+  display: ${(props) => (props.type === "sm" ? "flex" : "")};
   cursor: pointer;
   position: relative;
   flex-wrap: wrap;
@@ -27,25 +27,25 @@ const Container = styled.div`
   /* Mobile S [fixed]*/
   @media ${device.mobileS} {
     gap: 5px;
-    margin: ${(props) => (props.type === 'sm' ? '15px 0px' : '0px')};
-    max-width: ${(props) => (props.type === 'sm' ? '340px' : '300px')};
-    gap: ${(props) => (props.type === 'sm' ? '15px' : '0px')};
+    margin: ${(props) => (props.type === "sm" ? "15px 0px" : "0px")};
+    max-width: ${(props) => (props.type === "sm" ? "340px" : "300px")};
+    gap: ${(props) => (props.type === "sm" ? "15px" : "0px")};
   }
 
   /* Mobile M */
   @media ${device.mobileM} {
     gap: 5px;
-    margin: ${(props) => (props.type === 'sm' ? '15px 0px' : '0px')};
-    max-width: ${(props) => (props.type === 'sm' ? '340px' : '300px')};
-    gap: ${(props) => (props.type === 'sm' ? '15px' : '0px')};
+    margin: ${(props) => (props.type === "sm" ? "15px 0px" : "0px")};
+    max-width: ${(props) => (props.type === "sm" ? "340px" : "300px")};
+    gap: ${(props) => (props.type === "sm" ? "15px" : "0px")};
   }
 
   /* Mobile L */
   @media ${device.mobileL} {
     gap: 5px;
-    margin: ${(props) => (props.type === 'sm' ? '15px 0px' : '0px')};
-    max-width: ${(props) => (props.type === 'sm' ? '420px' : '300px')};
-    gap: ${(props) => (props.type === 'sm' ? '15px' : '0px')};
+    margin: ${(props) => (props.type === "sm" ? "15px 0px" : "0px")};
+    max-width: ${(props) => (props.type === "sm" ? "420px" : "300px")};
+    gap: ${(props) => (props.type === "sm" ? "15px" : "0px")};
   }
 
   /* Tablet */
@@ -88,8 +88,8 @@ const OptionsButton = styled.button`
 `;
 
 const Image = styled.img`
-  width: ${(props) => (props.type === 'sm' ? '15em' : '18em')};
-  height: ${(props) => (props.type === 'sm' ? '120px' : '202px')};
+  width: ${(props) => (props.type === "sm" ? "15em" : "18em")};
+  height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
   background-color: #999;
   flex: 1;
   border-radius: 10px 10px 5px 5px;
@@ -97,7 +97,7 @@ const Image = styled.img`
 
 const Details = styled.div`
   display: flex;
-  margin-top: ${(props) => (props.type === 'sm' ? '16px' : '0px')};
+  margin-top: ${(props) => (props.type === "sm" ? "16px" : "0px")};
   gap: 12px;
   font-family: Inter;
   flex: 1;
@@ -106,24 +106,24 @@ const Details = styled.div`
   /* Mobile S [fixed]*/
   @media ${device.mobileS} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
 
   /* Mobile M */
   @media ${device.mobileM} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
 
   /* Mobile L */
   @media ${device.mobileL} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
   /* Desktop */
   @media ${device.desktop} {
     gap: 5px;
-    margin-top: ${(props) => (props.type === 'sm' ? '0px' : '0px')};
+    margin-top: ${(props) => (props.type === "sm" ? "0px" : "0px")};
   }
 `;
 
@@ -132,22 +132,22 @@ const ChannelImage = styled.img`
   height: 36px;
   border-radius: 50%;
   background-color: #999;
-  display: ${(props) => props.type === 'sm' && 'none'};
+  display: ${(props) => props.type === "sm" && "none"};
 `;
 
 const Texts = styled.div`
   display: flex;
   flex-flow: column wrap;
-  min-width: ${(props) => (props.type === 'sm' ? '0px' : '300px')};
+  min-width: ${(props) => (props.type === "sm" ? "0px" : "250px")};
 
   /* Mobile S [fixed]*/
   @media ${device.mobileS} {
-    max-width: ${(props) => (props.type === 'sm' ? '200px' : '0px')};
+    max-width: ${(props) => (props.type === "sm" ? "200px" : "0px")};
   }
 `;
 
 const Title = styled.h1`
-  font-size: ${(props) => props.type !== 'sm' && '12px'};
+  font-size: ${(props) => props.type !== "sm" && "12px"};
   font-weight: 500;
   color: ${({ theme }) => theme.titleColor};
   display: flex;
@@ -175,8 +175,7 @@ const Info = styled.div`
 
 const ImgContainer = styled.div`
   position: relative;
-  justify-content: center;
-  align-content: center;
+  width: 100px;
 `;
 
 const Card = ({ type, video, currentUser }) => {
@@ -222,11 +221,12 @@ const Card = ({ type, video, currentUser }) => {
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       animate={{
-        border: 'none',
-        backgroundColor: 'transparent',
-        textAlign: 'left',
+        border: "none",
+        backgroundColor: "transparent",
+        textAlign: "left",
+        width: "260px",
       }}
-      transition={{ ease: 'easeOut', duration: 0.4 }}
+      transition={{ ease: "easeOut", duration: 0.4 }}
     >
       <Container
         type={type}
@@ -234,7 +234,7 @@ const Card = ({ type, video, currentUser }) => {
         // onMouseLeave={hoverOptions}
       >
         <OptionContainer>
-          {type === 'profile' && channel?._id === currentUser?._id ? (
+          {type === "profile" && channel?._id === currentUser?._id ? (
             hoverState ? (
               <>
                 <Options>
@@ -243,13 +243,13 @@ const Card = ({ type, video, currentUser }) => {
                 </Options>
               </>
             ) : (
-              ''
+              ""
             )
           ) : (
-            ''
+            ""
           )}
         </OptionContainer>
-        <Link to={`/video/${video?._id}`} style={{ textDecoration: 'none' }}>
+        <Link to={`/video/${video?._id}`} style={{ textDecoration: "none" }}>
           <ImgContainer>
             <Image type={type} src={video?.imgUrl} />
           </ImgContainer>
